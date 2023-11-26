537cfe01e94b2f84e8eba43435fbb1c368273706
samsul kobir <makkobir79@gmail.com>
2023-11-26 02:27:31 +0200

    resolver and schema moved to gpl folder

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { db } from "./db.js";
-const typeDefs = `#graphql
-
- type Product {
-    id: ID
-    name: String
-    image: String
-    description: String
-    price: Float
-    quantity: Int
-    onStock: Boolean
-    categoryId: String
-    
- }
-
-  type Query {
-    products: [Product]
-  }
-`;
-const resolvers = {
-    Query: {
-        products: () => db.products,
-    },
-};
+import { typeDefs } from "./gql/schema/index.js";
+import { resolvers } from "./gql/resolver/index.js";
 const server = new ApolloServer({
     typeDefs,
     resolvers,
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { db } from "./db.js";
-
-const typeDefs = `#graphql
-
- type Product {
-    id: ID
-    name: String
-    image: String
-    description: String
-    price: Float
-    quantity: Int
-    onStock: Boolean
-    categoryId: String
-    
- }
-
-  type Query {
-    products: [Product]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    products: () => db.products,
-  },
-};
+import { typeDefs } from "./gql/schema/index.js";
+import { resolvers } from "./gql/resolver/index.js";
 
 const server = new ApolloServer({
   typeDefs,
